31a09590afd3942097ebaa741427b907f776ce0f
sharonpraju <47088116+sharonpraju@users.noreply.github.com>
2019-11-22 21:03:27 +0530

    Update inventory_status.php

--- inventory_status.php
+++ inventory_status.php
@@ -15,11 +15,14 @@ $section=$_SESSION['section'];
 $admin=$_SESSION['admin'];
 $out_of_stock=0;
 
-$sql = "SELECT * FROM inv_stock WHERE total_units<='0' AND section_id='$section' AND status='1'";
+$sql = "SELECT item_name FROM inv_stock WHERE total_units<='0' AND section_id='$section' AND status='1'";
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result);
 do{
-    $out_of_stock++;
+    if($row['item_name']!=NULL)
+    {
+        $out_of_stock++;
+    }
 }while ($row = $result->fetch_assoc());
 
 echo'
