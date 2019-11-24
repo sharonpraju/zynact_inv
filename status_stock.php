1c0a44c72fb3bb45b403bfd7917e2cd2a44a6f33
Tibin Sunny <33937416+Tibinsunny@users.noreply.github.com>
2019-11-24 22:22:00 +0530

    Updated

--- status_stock.php
+++ status_stock.php
@@ -1,8 +1,11 @@
 <?php
 include 'db_connection.php';
 $conn = OpenCon();
+session_start();
+$section=$_SESSION['section'];
+
 echo "<body bgcolor=white>";
-$sql="SELECT * FROM inv_stock where status=1";
+$sql="SELECT * FROM inv_stock where status=1 and section_id='$section' ";
 if($result = mysqli_query($conn, $sql))
 {
         while($row = mysqli_fetch_array($result))
